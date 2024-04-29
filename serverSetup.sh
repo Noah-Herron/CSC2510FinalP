58095f16dc00d5e2ca0842de383cd06849e51a4b
Noah-Herron <125769187+Noah-Herron@users.noreply.github.com>
2024-04-29 12:25:43 -0500

    Added variables for each individual softwarepackage

--- serverSetup.sh
+++ serverSetup.sh
@@ -38,27 +38,44 @@ strStartDate=$(date +"%d-%b-%Y %R")
 echo ${strStartDate}
 
 while [ $intIndex -lt $intLength ]; do
-	intTickID=$(echo ${arrResults} |jq .[${intIndex}].ticketID)
+	intTickID=$(echo ${arrResults} | jq .[${intIndex}].ticketID)
 	# debug to test strPackLength
-	echo ${strPackLength}
+	#echo ${strPackLength}
 
 	# getting the requestors name
 	strReqName=$(echo ${arrResults} | jq .[${intIndex}].requestor)
-
 	# debug statment to make sure requestor name is correct
-	echo ${strReqName}
+	#echo ${strReqName}
 
 	# getting the requestors name
 	strconfig=$(echo ${arrResults} | jq .[${intIndex}].standardConfig)
-
 	# debug statment to make sure Config name is correct
-	echo ${strConfig}
+	#echo ${strConfig}
 
-	strSoftwarePackages=$(echo "$arrResults" | jq -r .[$intIndex].softwarePackages)
+	strSoftwarePackages=$(echo "$arrResults" | jq -r .[${intIndex}].softwarePackages)
 	numPackages=$(echo "$strSoftwarePackages" | jq length)
 	# debug to test number of packages is correct
-	echo ${numPackages}
+	#echo ${numPackages}
+
+	strHostName=$(whoami)
+	# debug to test if host name is correct
+	#echo ${strHostName}
+
+	while [ $intSecIndex -lt $numPackages ]; do
+                strPackage=$(echo ${strSoftwarePackages} | jq .[${intSecIndex}]."install")
+                echo ${strPackage}
+                ((intSecIndex++))
+        done
+
+	#installing packages
+#	sudo apt-get update
+#	sudo apt-get install apache2 -y
+#	sudo apt-get install mariadb-server -y
+#	sudo apt-get install neofetch -y
+
+
 
 
 	((intIndex++))
 done
+
