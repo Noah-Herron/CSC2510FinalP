ec76de27ac54887f263fa4aba4daea198226f2d7
Noah-Herron <125769187+Noah-Herron@users.noreply.github.com>
2024-04-29 13:06:15 -0500

    Added while loop to download software packages, and and if statement to only get the ticket that we want

--- serverSetup.sh
+++ serverSetup.sh
@@ -39,50 +39,41 @@ echo ${strStartDate}
 
 while [ $intIndex -lt $intLength ]; do
         intTickID=$(echo ${arrResults} | jq .[${intIndex}].ticketID)
-        # debug to test strPackLength
-        #echo ${strPackLength}
 
-        # getting the requestors name
-        strReqName=$(echo ${arrResults} | jq .[${intIndex}].requestor)
-        # debug statment to make sure requestor name is correct
-        #echo ${strReqName}
+	if [ $intTickID = $ticketID ]; then
 
-        # getting the requestors name
-        strconfig=$(echo ${arrResults} | jq .[${intIndex}].standardConfig)
-        # debug statment to make sure Config name is correct
-        #echo ${strConfig}
+	        # getting the requestors name
+        	strReqName=$(echo ${arrResults} | jq .[${intIndex}].requestor)
+	        # debug statment to make sure requestor name is correct
+        	#echo ${strReqName}
 
-        strSoftwarePackages=$(echo "$arrResults" | jq -r .[${intIndex}].softwarePackages)
-        numPackages=$(echo "$strSoftwarePackages" | jq length)
-        # debug to test number of packages is correct
-        echo ${numPackages}
+        	# getting the requestors name
+	        strconfig=$(echo ${arrResults} | jq .[${intIndex}].standardConfig)
+        	# debug statment to make sure Config name is correct
+        	#echo ${strConfig}
 
-        strHostName=$(whoami)
+        	strSoftwarePackages=$(echo "$arrResults" | jq -r .[${intIndex}].softwarePackages)
+        	numPackages=$(echo "$strSoftwarePackages" | jq length)
+        	# debug to test number of packages is correct
+        	echo ${numPackages}
 
-        # debug to test if host name is correct
-         #echo ${strHostName}
-        while [ $intSecIndex -lt $numPackages ]; do
-                strPackage=$(echo ${strSoftwarePackages} | jq .[${intSecIndex}].install)
-                #echo ${strPackage}
-                # removing the parenthases from th string
-                strPackage=${strPackage:1:-1}
-                # debug testing if correctly removed the parenthases
-                #echo $strPackage
-                sudo apt-get update
-                sudo apt-get install $strPackage -y
+	        strHostName=$(whoami)
 
-                ((intSecIndex++))
-        done
+        	# debug to test if host name is correct
+         	#echo ${strHostName}
+        	while [ $intSecIndex -lt $numPackages ]; do
+                	strPackage=$(echo ${strSoftwarePackages} | jq .[${intSecIndex}].install)
+                	#echo ${strPackage}
+                	# removing the parenthases from th string
+                	strPackage=${strPackage:1:-1}
+                	# debug testing if correctly removed the parenthases
+                	#echo $strPackage
+                	sudo apt-get update
+                	sudo apt-get install $strPackage -y
 
-
-        #installing packages
-#       sudo apt-get update
- #       sudo apt-get install apache2 -y
-  #      sudo apt-get install mariadb-server -y
-   #     sudo apt-get install neofetch -y
-
-#       echo $strSoftwarePackages
-
+                	((intSecIndex++))
+        	done
+	fi
 
         ((intIndex++))
 done
