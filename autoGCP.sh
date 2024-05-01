f8d0955139de26d15206685578eab738bb63648c
Noah-Herron <125769187+Noah-Herron@users.noreply.github.com>
2024-04-30 22:44:50 -0500

    Changed the file path to .ssh/gcp and changed to #! at the top of file for windows

--- autoGCP.sh
+++ autoGCP.sh
@@ -1,4 +1,5 @@
-#! bin/bash/
+#! /usr/bin/env bash
+# ^^^Windows ^^^
 #
 # Description: Copies and runs automation script on GCP remote server
 # Author: Noah Herron
@@ -11,11 +12,11 @@ eval "$(ssh-agent -s)"
 
 # The location of my gcp ssh key is in the .ssh directory
 # My gcp ssh key is called gcp
-ssh-add .ssh/gcp
+ssh-add ~/.ssh/gcp
 
 # Using scp, copies a shell script called serverSetup.sh from the
 # current directory to the home directory on the remote server
 # Note the two lines below is actually one line
-scp -i .ssh/gcp serverSetup.sh "${strUsername}"@"${strIP}":/home/"${strUsername}"
+scp -i ~/.ssh/gcp serverSetup.sh "${strUsername}"@"${strIP}":/home/"${strUsername}"
 ssh ${strUsername}@${strIP} "chmod 755 serverSetup.sh"
 ssh ${strUsername}@${strIP} "./serverSetup.sh ${strIP} ${strTicketID}"
